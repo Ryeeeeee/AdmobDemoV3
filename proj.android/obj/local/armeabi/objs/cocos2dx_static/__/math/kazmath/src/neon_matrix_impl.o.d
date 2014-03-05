36800804f9ea6ce74d5dc632e622c8a26adf9165
Ryeeeeee <spatblan@gmail.com>
2014-03-05 18:37:52 +0800

    Delete obj & libs

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/__/math/kazmath/src/neon_matrix_impl.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/__/math/kazmath/src/neon_matrix_impl.o: \
- /Users/su1livan/Desktop/cocos2d-x-3.0beta2/projects/AdmobDemoV3/proj.android/../cocos2d/cocos/2d/../math/kazmath/src/neon_matrix_impl.c \
- /Users/su1livan/Desktop/cocos2d-x-3.0beta2/projects/AdmobDemoV3/proj.android/../cocos2d/cocos/2d/../math/kazmath/include/kazmath/neon_matrix_impl.h
-
-/Users/su1livan/Desktop/cocos2d-x-3.0beta2/projects/AdmobDemoV3/proj.android/../cocos2d/cocos/2d/../math/kazmath/include/kazmath/neon_matrix_impl.h:
--- proj.android/obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o: \
- /Users/su1livan/Desktop/cocos2d-x-3.0beta2/projects/AdmobDemoV3/proj.android/../cocos2d/cocos/2d/ccFPSImages.c \
- /Users/su1livan/Desktop/cocos2d-x-3.0beta2/projects/AdmobDemoV3/proj.android/../cocos2d/cocos/2d/ccFPSImages.h
-
-/Users/su1livan/Desktop/cocos2d-x-3.0beta2/projects/AdmobDemoV3/proj.android/../cocos2d/cocos/2d/ccFPSImages.h:
